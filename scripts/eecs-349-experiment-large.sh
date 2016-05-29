5c503d84d3d32c116159deaee5c1a1821795d599
HERMANNITY <junhanliu2015@u.northwestern.edu>
2016-05-29 17:17:14 -0500

    can see the port

--- scripts/eecs-349-experiment-large.sh
+++ scripts/eecs-349-experiment-large.sh
@@ -2,7 +2,7 @@ python train.py \
        --data_file=data/my.txt \
        --dropout=0.5 \
        --num_epochs=5 \
-       --hidden_size= 8\
+       --hidden_size=8 \
        --num_layers=1 \
        --model="rnn" \
        --batch_size=64 \
