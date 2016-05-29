d06c300d72079176a7caaa85ac25ba8ed1cad845
HERMANNITY <junhanliu2015@u.northwestern.edu>
2016-05-29 17:19:45 -0500

    can see the port

--- scripts/eecs-349-experiment-large.sh
+++ scripts/eecs-349-experiment-large.sh
@@ -1,11 +1,11 @@
 python train.py \
        --data_file=data/my.txt \
        --dropout=0.5 \
-       --num_epochs=5 \
-       --hidden_size=8 \
-       --num_layers=1 \
+       --num_epochs=50 \
+       --hidden_size=128 \
+       --num_layers=2 \
        --model="rnn" \
-       --batch_size=64 \
+       --batch_size=100 \
        --output_dir=large \
 
 tensorboard --logdir=large/tensorboard_log/ --port=6007
